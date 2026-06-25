893c43dbc5764c18f163b53559f0f656866bfcfd
Christophe Henner <christophehenner@gmail.com>
2026-06-25 06:58:45 +0200

    Use Toolhub author search for by pages

--- public_html/lib/core/author-index.js
+++ public_html/lib/core/author-index.js
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-import { loadAllTools } from "./similarity.js";
-import { memoizeAsync, normStr } from "./util.js";
+import { normalizeTool, paginate } from "./api.js";
+import { normStr } from "./util.js";
 
 function authorRecords(tool) {
 	if (tool.authorObjs && tool.authorObjs.length) return tool.authorObjs;
@@ -13,28 +13,22 @@ export function authorProfileUrl(profile) {
 	return null;
 }
 
-function buildAuthorIndex(tools) {
-	const index = new Map();
+function entryFromTools(requestedName, tools) {
+	const key = normStr(requestedName);
+	const entry = { name: requestedName, tools: tools || [], profile: {} };
 	for (const tool of tools || []) {
-		const seenForTool = new Set();
 		for (const author of authorRecords(tool)) {
-			const name = author && author.name;
-			const key = normStr(name);
-			if (!key || seenForTool.has(key)) continue;
-			seenForTool.add(key);
-			if (!index.has(key)) index.set(key, { name, tools: [], profile: {} });
-			const entry = index.get(key);
-			if (!entry.tools.some((t) => t.name === tool.name)) entry.tools.push(tool);
+			if (normStr(author && author.name) !== key) continue;
+			entry.name = author.name || requestedName;
 			if (!entry.profile.url && author.url) entry.profile.url = author.url;
 			if (!entry.profile.wikiUsername && author.wikiUsername) entry.profile.wikiUsername = author.wikiUsername;
 		}
 	}
-	return index;
+	return entry;
 }
 
-export const authorIndex = memoizeAsync(() => loadAllTools().then(buildAuthorIndex));
-
 export async function toolsByAuthor(name) {
-	const index = await authorIndex();
-	return index.get(normStr(name)) || { name, tools: [], profile: {} };
+	const tools = await paginate("/search/tools/", { author__term: name, ordering: "-score" }, { pageSize: 100, maxPages: 50, map: normalizeTool })
+		.catch(() => []);
+	return entryFromTools(name, tools);
 }
